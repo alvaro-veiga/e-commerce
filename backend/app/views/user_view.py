9d35ec2010bec01aa5291c6d4cd419d0dae60bfe
Alvaro Veiga <alvarojoao17@gmail.com>
2025-03-11 14:13:10 -0300

    create user crud

--- backend/app/views/user_view.py
+++ backend/app/views/user_view.py
@@ -48,3 +48,49 @@ def getRoutes(request):
         '/api/users/profile'
     ]
     return Response(routes)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name= data['name'],
+            username = data['email'],
+            password = make_password(data['password']),
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+
+        return Response(serializer.data)
+    except:
+        message = { "atenção": "Este usuário ja está cadastrado no sistema"}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many = False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many = False)
+    data = request.data
+    user.first_name = data['name'] 
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != "":
+        user.password = make_password(data['password'])
+    
+    user.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteUserProfile(request, pk):
+    user_to_delete = User.objects.get(id=pk)
+    user_to_delete.delete
+    return Response('Usuário deletado com sucesso')
